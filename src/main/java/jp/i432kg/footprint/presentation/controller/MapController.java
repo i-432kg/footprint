c8656fc3826bb0ff0b9a342c1a7fcf42fbbd4e68
432kg <432kg.biz@gmail.com>
2026-02-05 22:43:32 +0900

    マップ画面を簡易実装

---
+++ src/main/java/jp/i432kg/footprint/presentation/controller/MapController.java
@@ -0,0 +1,13 @@
+package jp.i432kg.footprint.presentation.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MapController {
+
+    @GetMapping("/map")
+    public String map() {
+        return "map";
+    }
+}
