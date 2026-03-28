6aaa4f523e2b1e337ddf2ed4c78020c8920e1a59
432kg <432kg.biz@gmail.com>
2026-03-28 16:18:36 +0900

    flywayの依存関係を追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -53,6 +53,8 @@ dependencies {
     implementation("software.amazon.awssdk:s3")
     implementation("org.flywaydb:flyway-core")
     implementation("org.flywaydb:flyway-mysql")
+    implementation("org.springframework.boot:spring-boot-starter-flyway")
+    implementation("org.springframework.boot:spring-boot-starter-flyway-test")
 }
 
 tasks.withType<Test> {
