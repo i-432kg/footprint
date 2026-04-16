e567dbe7c477ed2c858abd4d6d430313535a0964
432kg <432kg.biz@gmail.com>
2026-04-16 18:39:01 +0900

    nullチェック漏れの修正

--- src/main/java/jp/i432kg/footprint/domain/value/StorageType.java
+++ src/main/java/jp/i432kg/footprint/domain/value/StorageType.java
@@ -15,15 +15,22 @@ public enum StorageType {
     LOCAL("LOCAL"),
     S3("S3");
 
+    static String FIELD_NAME = "storage_type";
+
     private final String value;
 
     public static StorageType of(final @Nullable String value) {
+
+        if (Objects.isNull(value)) {
+            throw InvalidValueException.required(FIELD_NAME);
+        }
+
         return Arrays.stream(values())
                 .filter(v -> v.value.equalsIgnoreCase(value))
                 .findFirst()
                 .orElseThrow(() -> InvalidValueException.invalid(
-                        "storage_type",
-                        Objects.requireNonNull(value),
+                        FIELD_NAME,
+                        value,
                         "unknown storage type"
                 ));
     }
