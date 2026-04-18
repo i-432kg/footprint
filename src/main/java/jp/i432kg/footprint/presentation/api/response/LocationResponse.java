271af53e5e71134f5a35873295fed85d7df33a23
432kg <432kg.biz@gmail.com>
2026-04-18 23:15:18 +0900

    不足していたNullableを明示

--- src/main/java/jp/i432kg/footprint/presentation/api/response/LocationResponse.java
+++ src/main/java/jp/i432kg/footprint/presentation/api/response/LocationResponse.java
@@ -2,6 +2,7 @@ package jp.i432kg.footprint.presentation.api.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Value;
+import org.jspecify.annotations.Nullable;
 
 /**
  * 投稿に紐づく位置情報を表す API レスポンス DTO です。
@@ -13,10 +14,12 @@ public class LocationResponse {
     /**
      * 緯度です。
      */
+    @Nullable
     Double lat;
 
     /**
      * 経度です。
      */
+    @Nullable
     Double lng;
 }
